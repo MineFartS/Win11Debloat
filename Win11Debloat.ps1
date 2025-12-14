5181805c79d12a66aca9a04db94226da47ba43c5
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-14 04:16:46 -0800

    Update Win11Debloat.ps1

--- Win11Debloat.ps1
+++ Win11Debloat.ps1
@@ -1,294 +1,62 @@
 #Requires -RunAsAdministrator
 
-# RemoveApps 
-$appsList = ReadAppslistFromFile "$PSScriptRoot/Appslist.txt" 
+# ==============================================================================================================================
+# Remove Apps
 
+# Raw list of apps
+$rApps = (Get-Content -Path "$PSScriptRoot/Appslist.txt")
 
-Write-Output "> Removing $($appsList.Count) apps..."
-RemoveApps $appsList
+# Get list of apps from file at the path provided, and remove them one by one
+Foreach ($line in $rApps.Split('\n')) { 
     
-ForceRemoveEdge
+    $app = ($line.Split('#')[0].Trim())
 
+    Write-Output "Attempting to remove $app..."
 
-    'DisableDVR' {
-        RegImport "> Disabling Xbox game/screen recording..." "Disable_DVR.reg"
-        continue
-    }
-    'DisableGameBarIntegration' {
-        RegImport "> Disabling Game Bar integration..." "Disable_Game_Bar_Integration.reg"
-        continue
-    }
-    'DisableTelemetry' {
-        RegImport "> Disabling telemetry, diagnostic data, activity history, app-launch tracking and targeted ads..." "Disable_Telemetry.reg"
-        continue
-    }
-    {$_ -in "DisableSuggestions", "DisableWindowsSuggestions"} {
-        RegImport "> Disabling tips, tricks, suggestions and ads across Windows..." "Disable_Windows_Suggestions.reg"
-        continue
-    }
-    'DisableEdgeAds' {
-        RegImport "> Disabling ads, suggestions and the MSN news feed in Microsoft Edge..." "Disable_Edge_Ads_And_Suggestions.reg"
-        continue
-    }
-    {$_ -in "DisableLockscrTips", "DisableLockscreenTips"} {
-        RegImport "> Disabling tips & tricks on the lockscreen..." "Disable_Lockscreen_Tips.reg"
-        continue
-    }
-    'DisableDesktopSpotlight' {
-        RegImport "> Disabling the 'Windows Spotlight' desktop background option..." "Disable_Desktop_Spotlight.reg"
-        continue
-    }
-    'DisableSettings365Ads' {
-        RegImport "> Disabling Microsoft 365 ads in Settings Home..." "Disable_Settings_365_Ads.reg"
-        continue
-    }
-    'DisableSettingsHome' {
-        RegImport "> Disabling the Settings Home page..." "Disable_Settings_Home.reg"
-        continue
-    }
-    {$_ -in "DisableBingSearches", "DisableBing"} {
-        RegImport "> Disabling Bing web search, Bing AI and Cortana from Windows search..." "Disable_Bing_Cortana_In_Search.reg"
-        
-        # Also remove the app package for Bing search
-        $appsList = 'Microsoft.BingSearch'
-        RemoveApps $appsList
-        continue
-    }
-    'DisableCopilot' {
-        RegImport "> Disabling Microsoft Copilot..." "Disable_Copilot.reg"
+    #
+    if (($app -eq "Microsoft.OneDrive") -or ($app -eq "Microsoft.Edge")) {
 
-        # Also remove the app package for Copilot
-        $appsList = 'Microsoft.Copilot'
-        RemoveApps $appsList
-        continue
-    }
-    'DisableRecall' {
-        RegImport "> Disabling Windows Recall..." "Disable_AI_Recall.reg"
-        continue
-    }
-    'DisableClickToDo' {
-        RegImport "> Disabling Click to Do..." "Disable_Click_to_Do.reg"
-        continue
-    }
-    'DisableEdgeAI' {
-        RegImport "> Disabling AI features in Microsoft Edge..." "Disable_Edge_AI_Features.reg"
-        continue
-    }
-    'DisablePaintAI' {
-        RegImport "> Disabling AI features in Paint..." "Disable_Paint_AI_Features.reg"
-        continue
-    }
-    'DisableNotepadAI' {
-        RegImport "> Disabling AI features in Notepad..." "Disable_Notepad_AI_Features.reg"
-        continue
-    }
-    'RevertContextMenu' {
-        RegImport "> Restoring the old Windows 10 style context menu..." "Disable_Show_More_Options_Context_Menu.reg"
-        continue
-    }
-    'DisableMouseAcceleration' {
-        RegImport "> Turning off Enhanced Pointer Precision..." "Disable_Enhance_Pointer_Precision.reg"
-        continue
-    }
-    'DisableStickyKeys' {
-        RegImport "> Disabling the Sticky Keys keyboard shortcut..." "Disable_Sticky_Keys_Shortcut.reg"
-        continue
-    }
-    'DisableFastStartup' {
-        RegImport "> Disabling Fast Start-up..." "Disable_Fast_Startup.reg"
-        continue
-    }
-    'DisableModernStandbyNetworking' {
-        RegImport "> Disabling network connectivity during Modern Standby..." "Disable_Modern_Standby_Networking.reg"
-        continue
-    }
-    'ClearStart' {
-        Write-Output "> Removing all pinned apps from the start menu for user $(GetUserName)..."
-        ReplaceStartMenu
-        Write-Output ""
-        continue
-    }
-    'ReplaceStart' {
-        Write-Output "> Replacing the start menu for user $(GetUserName)..."
-        ReplaceStartMenu $script:Params.Item("ReplaceStart")
-        Write-Output ""
-        continue
-    }
-    'ClearStartAllUsers' {
-        ReplaceStartMenuForAllUsers
-        continue
-    }
-    'ReplaceStartAllUsers' {
-        ReplaceStartMenuForAllUsers $script:Params.Item("ReplaceStartAllUsers")
-        continue
-    }
-    'DisableStartRecommended' {
-        RegImport "> Disabling the start menu recommended section..." "Disable_Start_Recommended.reg"
-        continue
-    }
-    'DisableStartPhoneLink' {
-        RegImport "> Disabling the Phone Link mobile devices integration in the start menu..." "Disable_Phone_Link_In_Start.reg"
-        continue
-    }
-    'EnableDarkMode' {
-        RegImport "> Enabling dark mode for system and apps..." "Enable_Dark_Mode.reg"
-        continue
-    }
-    'DisableTransparency' {
-        RegImport "> Disabling transparency effects..." "Disable_Transparency.reg"
-        continue
-    }
-    'DisableAnimations' {
-        RegImport "> Disabling animations and visual effects..." "Disable_Animations.reg"
-        continue
-    }
-    'TaskbarAlignLeft' {
-        RegImport "> Aligning taskbar buttons to the left..." "Align_Taskbar_Left.reg"
-        continue
-    }
-    'CombineTaskbarAlways' {
-        RegImport "> Setting the taskbar on the main display to always combine buttons and hide labels..." "Combine_Taskbar_Always.reg"
-        continue
-    }
-    'CombineTaskbarWhenFull' {
-        RegImport "> Setting the taskbar on the main display to only combine buttons and hide labels when the taskbar is full..." "Combine_Taskbar_When_Full.reg"
-        continue
-    }
-    'CombineTaskbarNever' {
-        RegImport "> Setting the taskbar on the main display to never combine buttons or hide labels..." "Combine_Taskbar_Never.reg"
-        continue
-    }
-    'CombineMMTaskbarAlways' {
-        RegImport "> Setting the taskbar on secondary displays to always combine buttons and hide labels..." "Combine_MMTaskbar_Always.reg"
-        continue
-    }
-    'CombineMMTaskbarWhenFull' {
-        RegImport "> Setting the taskbar on secondary displays to only combine buttons and hide labels when the taskbar is full..." "Combine_MMTaskbar_When_Full.reg"
-        continue
-    }
-    'CombineMMTaskbarNever' {
-        RegImport "> Setting the taskbar on secondary displays to never combine buttons or hide labels..." "Combine_MMTaskbar_Never.reg"
-        continue
-    }
-    'MMTaskbarModeAll' {
-        RegImport "> Setting the taskbar to only show app icons on main taskbar..." "MMTaskbarMode_All.reg"
-        continue
-    }
-    'MMTaskbarModeMainActive' {
-        RegImport "> Setting the taskbar to show app icons on all taskbars..." "MMTaskbarMode_Main_Active.reg"
-        continue
-    }
-    'MMTaskbarModeActive' {
-        RegImport "> Setting the taskbar to only show app icons on the taskbar where the window is open..." "MMTaskbarMode_Active.reg"
-        continue
-    }
-    'HideSearchTb' {
-        RegImport "> Hiding the search icon from the taskbar..." "Hide_Search_Taskbar.reg"
-        continue
-    }
-    'ShowSearchIconTb' {
-        RegImport "> Changing taskbar search to icon only..." "Show_Search_Icon.reg"
-        continue
-    }
-    'ShowSearchLabelTb' {
-        RegImport "> Changing taskbar search to icon with label..." "Show_Search_Icon_And_Label.reg"
-        continue
-    }
-    'ShowSearchBoxTb' {
-        RegImport "> Changing taskbar search to search box..." "Show_Search_Box.reg"
-        continue
-    }
-    'HideTaskview' {
-        RegImport "> Hiding the taskview button from the taskbar..." "Hide_Taskview_Taskbar.reg"
-        continue
-    }
-    {$_ -in "HideWidgets", "DisableWidgets"} {
-        RegImport "> Disabling widgets on the taskbar & lockscreen..." "Disable_Widgets_Service.reg"
+        winget.exe `
+            uninstall `
+            --accept-source-agreements `
+            --disable-interactivity `
+            --id $app
 
-        # Also remove the app package for Widgets
-        $appsList = 'Microsoft.StartExperiencesApp'
-        RemoveApps $appsList
-        continue
+    #
+    } else {
+            
+        Get-AppxPackage `
+            -Name "*$app*" `
+            -AllUsers `
+        | Remove-AppxPackage `
+            -AllUsers `
+            -ErrorAction Continue
+
     }
-    {$_ -in "HideChat", "DisableChat"} {
-        RegImport "> Hiding the chat icon from the taskbar..." "Disable_Chat_Taskbar.reg"
-        continue
+
+    # Remove provisioned app from OS image, so the app won't be installed for any new users
+    Get-AppxProvisionedPackage -Online `
+    | Where-Object PackageName -like $app `
+    | ForEach-Object { 
+        Remove-ProvisionedAppxPackage `
+        -Online -AllUsers `
+        -PackageName $_.PackageName 
     }
-    'EnableEndTask' {
-        RegImport "> Enabling the 'End Task' option in the taskbar right click menu..." "Enable_End_Task.reg"
-        continue
-    }
-    'EnableLastActiveClick' {
-        RegImport "> Enabling the 'Last Active Click' behavior in the taskbar app area..." "Enable_Last_Active_Click.reg"
-        continue
-    }
-    'ExplorerToHome' {
-        RegImport "> Changing the default location that File Explorer opens to `Home`..." "Launch_File_Explorer_To_Home.reg"
-        continue
-    }
-    'ExplorerToThisPC' {
-        RegImport "> Changing the default location that File Explorer opens to `This PC`..." "Launch_File_Explorer_To_This_PC.reg"
-        continue
-    }
-    'ExplorerToDownloads' {
-        RegImport "> Changing the default location that File Explorer opens to `Downloads`..." "Launch_File_Explorer_To_Downloads.reg"
-        continue
-    }
-    'ExplorerToOneDrive' {
-        RegImport "> Changing the default location that File Explorer opens to `OneDrive`..." "Launch_File_Explorer_To_OneDrive.reg"
-        continue
-    }
-    'ShowHiddenFolders' {
-        RegImport "> Unhiding hidden files, folders and drives..." "Show_Hidden_Folders.reg"
-        continue
-    }
-    'ShowKnownFileExt' {
-        RegImport "> Enabling file extensions for known file types..." "Show_Extensions_For_Known_File_Types.reg"
-        continue
-    }
-    'HideHome' {
-        RegImport "> Hiding the home section from the File Explorer navigation pane..." "Hide_Home_from_Explorer.reg"
-        continue
-    }
-    'HideGallery' {
-        RegImport "> Hiding the gallery section from the File Explorer navigation pane..." "Hide_Gallery_from_Explorer.reg"
-        continue
-    }
-    'HideDupliDrive' {
-        RegImport "> Hiding duplicate removable drive entries from the File Explorer navigation pane..." "Hide_duplicate_removable_drives_from_navigation_pane_of_File_Explorer.reg"
-        continue
-    }
-    {$_ -in "HideOnedrive", "DisableOnedrive"} {
-        RegImport "> Hiding the OneDrive folder from the File Explorer navigation pane..." "Hide_Onedrive_Folder.reg"
-        continue
-    }
-    {$_ -in "Hide3dObjects", "Disable3dObjects"} {
-        RegImport "> Hiding the 3D objects folder from the File Explorer navigation pane..." "Hide_3D_Objects_Folder.reg"
-        continue
-    }
-    {$_ -in "HideMusic", "DisableMusic"} {
-        RegImport "> Hiding the music folder from the File Explorer navigation pane..." "Hide_Music_folder.reg"
-        continue
-    }
-    {$_ -in "HideIncludeInLibrary", "DisableIncludeInLibrary"} {
-        RegImport "> Hiding 'Include in library' in the context menu..." "Disable_Include_in_library_from_context_menu.reg"
-        continue
-    }
-    {$_ -in "HideGiveAccessTo", "DisableGiveAccessTo"} {
-        RegImport "> Hiding 'Give access to' in the context menu..." "Disable_Give_access_to_context_menu.reg"
-        continue
-    }
-    {$_ -in "HideShare", "DisableShare"} {
-        RegImport "> Hiding 'Share' in the context menu..." "Disable_Share_from_context_menu.reg"
-        continue
-    }
+
 }
 
-RestartExplorer
+# ==============================================================================================================================
+# Import Registry Keys
 
-Write-Output ""
-Write-Output ""
-Write-Output ""
-Write-Output "Script completed! Please check above for any errors."
+#
+Get-ChildItem -Path "$PSScriptRoot\Regfiles\" | ForEach-Object {
+    
+    #
+    reg import $_.FullName
 
-AwaitKeyToExit
+}
+
+# ==============================================================================================================================
+
+#
+Pause.exe
